ea5d22f0807c29968908e32ea9092d6367826caf
AleAro <aarouesty@pastpost.com>
2026-02-15 22:31:27 -0600

    Render API connection

--- Videogame/QuetzalUnity/Assets/Scripts/API/APIConfig.cs
+++ Videogame/QuetzalUnity/Assets/Scripts/API/APIConfig.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public static class APIConfig
 {
-    public static string BaseUrl = "http://localhost:8000";
+    public static string BaseUrl = "https://quetzal-api.onrender.com";
 }
